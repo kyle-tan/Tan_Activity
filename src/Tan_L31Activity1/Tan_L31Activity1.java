28c51586a006db02134763d96461e73b53ebb787
kyle-tan <kyle.tan@awsys-i.com>
2024-09-03 14:22:45 +0800

    Added First Activity

---
+++ src/Tan_L31Activity1/Tan_L31Activity1.java
@@ -0,0 +1,22 @@
+package Tan_L31Activity1;
+
+public class Tan_L31Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Kyle Daeniel D. Tan";
+		int age = 23;
+		String country = "Philippines";
+		String sport = "Basketball";
+		double hours = 2.5;
+		String game = "Wordscapes";
+		String subject = "Math";
+		int grade = 95;
+		
+		System.out.println("My name is " +name+ ". I'm " +age+" years old and from the "+country);
+		System.out.println("My favorite sport is "+sport+". I play for "+hours+" hour(s) a day.");
+		System.out.println("I like to play "+game+ " when I'm tired.");
+		System.out.println("In scool, my favorite subject was " +subject+". I scored a "+grade+".");
+	}
+
+}
